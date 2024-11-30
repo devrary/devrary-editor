1f37e67f8636877628aa5e3905566660f25260f0
Resister-boy <hassanpumped17@gmail.com>
2024-12-01 04:23:24 +0900

    config: add wagmi project id

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,9 @@ const nextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
-    config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
 };
